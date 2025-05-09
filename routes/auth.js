38f21aebd4950b9039d7bfc0eb505113881b6d79
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2025-05-09 18:46:23 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,19 +3,22 @@ import {
   signup,
   login,
   getUserDetails,
-  verifyToken,
+  verifyFirebaseToken,
 } from "../controllers/auth.js";
 
 const router = express.Router();
 
-// Route: Send OTP
-
-// Route: Signup
+// Route for Signup
 router.post("/signup", signup);
 
-// Route: Login
+// Route for Login
 router.post("/login", login);
 
+// Protected Route that requires token verification
+router.get("/protected-route", verifyFirebaseToken, (req, res) => {
+  res.json({ message: "This is protected data", user: req.user });
+});
+// Route to get user details (you can make it a protected route too)
 router.get("/user", getUserDetails);
 
 export default router;
